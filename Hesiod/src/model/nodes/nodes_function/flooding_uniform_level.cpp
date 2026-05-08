c3f7d0ea811f45e41129ebe0a9d7d93af8ba6b32
Otto Link <otto.link.bv@gmail.com>
2026-05-08 11:51:02 +0200

    Refactor FloodingUniformLevel node

--- Hesiod/src/model/nodes/nodes_function/flooding_uniform_level.cpp
+++ Hesiod/src/model/nodes/nodes_function/flooding_uniform_level.cpp
@@ -14,39 +14,75 @@ using namespace attr;
 namespace hesiod
 {
 
+// -----------------------------------------------------------------------------
+// Ports & Attributes
+// -----------------------------------------------------------------------------
+
+constexpr const char *P_IN = "input";
+constexpr const char *P_OUT = "water_depth";
+
+constexpr const char *A_ELEVATION = "elevation";
+
+// -----------------------------------------------------------------------------
+// Setup
+// -----------------------------------------------------------------------------
+
 void setup_flooding_uniform_level_node(BaseNode &node)
 {
   Logger::log()->trace("setup node {}", node.get_label());
 
-  // port(s)
-  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, "input");
-  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, "water_depth", CONFIG(node));
+  // --- Ports
 
-  // attribute(s)
-  node.add_attr<FloatAttribute>("elevation", "elevation", 0.2f, -1.f, 2.f);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_IN);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, P_OUT, CONFIG(node));
+
+  // --- Attributes
+
+  // clang-format off
+  node.add_attr<FloatAttribute>(A_ELEVATION, "Elevation", 0.2f, -1.f, 2.f);
+  // clang-format on
+
+  // --- Attribute(s) order
+
+  node.set_attr_ordered_key(
+      {"_GROUPBOX_BEGIN_Main Parameters", A_ELEVATION, "_GROUPBOX_END_"});
 }
+
+// -----------------------------------------------------------------------------
+// Compute
+// -----------------------------------------------------------------------------
 
 void compute_flooding_uniform_level_node(BaseNode &node)
 {
   Logger::log()->trace("computing node [{}]/[{}]", node.get_label(), node.get_id());
 
-  hmap::VirtualArray *p_in = node.get_value_ref<hmap::VirtualArray>("input");
+  // --- Inputs / Outputs
 
-  if (p_in)
-  {
-    hmap::VirtualArray *p_out = node.get_value_ref<hmap::VirtualArray>("water_depth");
+  auto *p_in = node.get_value_ref<hmap::VirtualArray>(P_IN);
+  auto *p_out = node.get_value_ref<hmap::VirtualArray>(P_OUT);
 
-    hmap::for_each_tile(
-        {p_out, p_in},
-        [&node](std::vector<hmap::Array *> p_arrays, const hmap::TileRegion &)
-        {
-          auto [pa_out, pa_in] = unpack<2>(p_arrays);
-          *pa_out = hmap::flooding_uniform_level(
-              *pa_in,
-              node.get_attr<FloatAttribute>("elevation"));
-        },
-        node.cfg().cm_cpu);
-  }
+  if (!p_in)
+    return;
+
+  // --- Params
+
+  const auto elevation = node.get_attr<FloatAttribute>(A_ELEVATION);
+
+  // --- Compute
+
+  hmap::for_each_tile(
+      {p_in},
+      {p_out},
+      [&](std::vector<const hmap::Array *> in,
+          std::vector<hmap::Array *>       out,
+          const hmap::TileRegion &)
+      {
+        auto [pa_in] = unpack<1>(in);
+        auto [pa_out] = unpack<1>(out);
+
+        *pa_out = hmap::flooding_uniform_level(*pa_in, elevation);
+      },
+      node.cfg().cm_cpu);
 }
 
 } // namespace hesiod
