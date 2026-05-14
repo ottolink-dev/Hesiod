c1e2721d72598e551ddf1ea52042e2c040a49257
Otto Link <otto.link.bv@gmail.com>
2026-05-14 15:04:56 +0200

    Add reference input for Equalize node to allow histogram matching between two inputs

--- Hesiod/src/model/nodes/nodes_function/equalize.cpp
+++ Hesiod/src/model/nodes/nodes_function/equalize.cpp
@@ -15,18 +15,36 @@ using namespace attr;
 namespace hesiod
 {
 
+// -----------------------------------------------------------------------------
+// Ports
+// -----------------------------------------------------------------------------
+
+constexpr const char *P_IN = "input";
+constexpr const char *P_REF = "reference";
+constexpr const char *P_MASK = "mask";
+constexpr const char *P_OUT = "output";
+
+// -----------------------------------------------------------------------------
+// Setup
+// -----------------------------------------------------------------------------
+
 void setup_equalize_node(BaseNode &node)
 {
   Logger::log()->trace("setup node {}", node.get_label());
 
-  // port(s)
-  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, "input");
-  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, "mask");
-  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, "output", CONFIG(node));
+  // --- Ports
 
-  // attribute(s)
+  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_IN);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_REF);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_MASK);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, P_OUT, CONFIG(node));
 
-  // attribute(s) order
+  // --- Attributes
+
+  // none...
+
+  // --- Attributes order
+
   node.set_attr_ordered_key({});
 
   setup_pre_process_mask_attributes(node);
@@ -34,34 +52,73 @@ void setup_equalize_node(BaseNode &node)
                                           {.add_mix = true, .remap_active_state = false});
 }
 
+// -----------------------------------------------------------------------------
+// Compute
+// -----------------------------------------------------------------------------
+
 void compute_equalize_node(BaseNode &node)
 {
   Logger::log()->trace("computing node [{}]/[{}]", node.get_label(), node.get_id());
 
-  hmap::VirtualArray *p_in = node.get_value_ref<hmap::VirtualArray>("input");
+  // --- Inputs / Outputs
 
-  if (p_in)
+  auto *p_in = node.get_value_ref<hmap::VirtualArray>(P_IN);
+  auto *p_ref = node.get_value_ref<hmap::VirtualArray>(P_REF);
+  auto *p_mask = node.get_value_ref<hmap::VirtualArray>(P_MASK);
+  auto *p_out = node.get_value_ref<hmap::VirtualArray>(P_OUT);
+
+  if (!p_in)
+    return;
+
+  // --- Prepare mask
+
+  std::shared_ptr<hmap::VirtualArray> sp_mask = pre_process_mask(node, p_mask, *p_in);
+
+  // --- Compute
+
+  if (p_ref)
   {
-    hmap::VirtualArray *p_mask = node.get_value_ref<hmap::VirtualArray>("mask");
-    hmap::VirtualArray *p_out = node.get_value_ref<hmap::VirtualArray>("output");
-
-    // prepare mask
-    std::shared_ptr<hmap::VirtualArray> sp_mask = pre_process_mask(node, p_mask, *p_in);
-
     hmap::for_each_tile(
-        {p_out, p_in, p_mask},
-        [](std::vector<hmap::Array *> p_arrays, const hmap::TileRegion &)
+        {p_in, p_ref, p_mask},
+        {p_out},
+        [&](std::vector<const hmap::Array *> in,
+            std::vector<hmap::Array *>       out,
+            const hmap::TileRegion &)
         {
-          auto [pa_out, pa_in, pa_mask] = unpack<3>(p_arrays);
+          auto [pa_in, pa_ref, pa_mask] = unpack<3>(in);
+          auto [pa_out] = unpack<1>(out);
+
+          *pa_out = *pa_in;
+          hmap::match_histogram(*pa_out, *pa_ref);
+
+          // apply mask
+          if (pa_mask)
+            *pa_out = hmap::lerp(*pa_in, *pa_out, *pa_mask);
+        },
+        node.cfg().cm_single_array);
+  }
+  else
+  {
+    hmap::for_each_tile(
+        {p_in, p_mask},
+        {p_out},
+        [&](std::vector<const hmap::Array *> in,
+            std::vector<hmap::Array *>       out,
+            const hmap::TileRegion &)
+        {
+          auto [pa_in, pa_mask] = unpack<2>(in);
+          auto [pa_out] = unpack<1>(out);
+
           *pa_out = *pa_in;
 
           hmap::equalize(*pa_out, pa_mask);
         },
         node.cfg().cm_single_array);
-
-    // post-process
-    post_process_heightmap(node, *p_out, p_in);
   }
+
+  // --- Post-process
+
+  post_process_heightmap(node, *p_out, p_in);
 }
 
 } // namespace hesiod
