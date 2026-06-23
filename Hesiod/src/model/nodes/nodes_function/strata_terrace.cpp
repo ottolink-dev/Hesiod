28ccee00b77b6c51e12807ab674262e1a25dda80
Otto Link <otto.link.bv@gmail.com>
2026-06-23 20:33:58 +0200

    feat(nodes): Add slope and angle control to StrataTerrace node

--- Hesiod/src/model/nodes/nodes_function/strata_terrace.cpp
+++ Hesiod/src/model/nodes/nodes_function/strata_terrace.cpp
@@ -32,6 +32,8 @@ constexpr const char *A_GAMMA = "gamma";
 constexpr const char *A_SEED = "seed";
 constexpr const char *A_LINEAR_GAMMA = "linear_gamma";
 constexpr const char *A_GAMMA_NOISE_RATIO = "gamma_noise_ratio";
+constexpr const char *A_SLOPE = "slope";
+constexpr const char *A_ANGLE = "angle";
 
 // -----------------------------------------------------------------------------
 // Setup
@@ -41,30 +43,36 @@ void setup_strata_terrace_node(BaseNode &node)
 {
   Logger::log()->trace("setup node {}", node.get_label());
 
-  // port(s)
+  // --- Ports
+
   node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_IN);
   node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_NOISE);
   node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_MASK);
   node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, P_OUT, CONFIG(node));
 
-  // attribute(s)
+  // --- Attributes
+
+  // clang-format off
   node.add_attr<FloatAttribute>(A_KZ, "Number of Strata", 8.f, 0.f, FLT_MAX);
   node.add_attr<FloatAttribute>(A_GAMMA, "Profile Gamma", 0.5f, 0.01f, 2.f);
   node.add_attr<SeedAttribute>(A_SEED, "Seed");
   node.add_attr<BoolAttribute>(A_LINEAR_GAMMA, "Use Linear Terrace Profile", false);
-  node.add_attr<FloatAttribute>(A_GAMMA_NOISE_RATIO,
-                                "Gamma Noise Influence",
-                                0.5f,
-                                0.f,
-                                1.f);
+  node.add_attr<FloatAttribute>(A_GAMMA_NOISE_RATIO, "Gamma Noise Influence", 0.5f, 0.f, 1.f);
+  node.add_attr<FloatAttribute>(A_SLOPE, "Slope", 0.f, 0.f, 10.f);
+  node.add_attr<FloatAttribute>(A_ANGLE, "Angle", 0.f, -180.f, 180.f, "{:.1f}°");
+  // clang-format on
 
-  // attribute(s) order
+  // --- Attribute(s) order
+
   node.set_attr_ordered_key({"_GROUPBOX_BEGIN_Strata Quantization",
                              A_KZ,
                              A_GAMMA,
                              A_LINEAR_GAMMA,
                              "_GROUPBOX_END_",
-                             //
+                             "_GROUPBOX_BEGIN_Orientation / Rotation",
+                             A_SLOPE,
+                             A_ANGLE,
+                             "_GROUPBOX_END_",
                              "_GROUPBOX_BEGIN_Profile Variation",
                              A_SEED,
                              A_GAMMA_NOISE_RATIO,
@@ -83,33 +91,27 @@ void compute_strata_terrace_node(BaseNode &node)
 {
   Logger::log()->trace("computing node [{}]/[{}]", node.get_label(), node.get_id());
 
+  // --- Inputs / Outputs
+
   auto *p_in = node.get_value_ref<hmap::VirtualArray>(P_IN);
   auto *p_noise = node.get_value_ref<hmap::VirtualArray>(P_NOISE);
   auto *p_mask = node.get_value_ref<hmap::VirtualArray>(P_MASK);
   auto *p_out = node.get_value_ref<hmap::VirtualArray>(P_OUT);
 
-  if (!p_in)
+  if (!p_in || !p_out)
     return;
 
-  // --- Parameters wrapper
+  // --- Params
 
-  const auto params = [&node]()
-  {
-    struct P
-    {
-      float kz;
-      float gamma;
-      uint  seed;
-      bool  linear_gamma;
-      float gamma_noise_ratio;
-    };
-
-    return P{.kz = node.get_attr<FloatAttribute>(A_KZ),
-             .gamma = node.get_attr<FloatAttribute>(A_GAMMA),
-             .seed = node.get_attr<SeedAttribute>(A_SEED),
-             .linear_gamma = node.get_attr<BoolAttribute>(A_LINEAR_GAMMA),
-             .gamma_noise_ratio = node.get_attr<FloatAttribute>(A_GAMMA_NOISE_RATIO)};
-  }();
+  // clang-format off
+  const auto kz                = node.get_attr<FloatAttribute>(A_KZ);
+  const auto gamma             = node.get_attr<FloatAttribute>(A_GAMMA);
+  const auto seed              = node.get_attr<SeedAttribute>(A_SEED);
+  const auto linear_gamma      = node.get_attr<BoolAttribute>(A_LINEAR_GAMMA);
+  const auto gamma_noise_ratio = node.get_attr<FloatAttribute>(A_GAMMA_NOISE_RATIO);
+  const auto slope             = node.get_attr<FloatAttribute>(A_SLOPE);
+  const auto angle             = node.get_attr<FloatAttribute>(A_ANGLE);
+  // clang-format on
 
   // --- Prepare mask
 
@@ -120,29 +122,31 @@ void compute_strata_terrace_node(BaseNode &node)
   hmap::for_each_tile(
       {p_in, p_noise, p_mask},
       {p_out},
-      [&node, &params](std::vector<const hmap::Array *> p_arrays_in,
-                       std::vector<hmap::Array *>       p_arrays_out,
-                       const hmap::TileRegion &)
+      [&](std::vector<const hmap::Array *> in,
+          std::vector<hmap::Array *>       out,
+          const hmap::TileRegion &)
       {
-        auto [pa_in, pa_noise, pa_mask] = unpack<3>(p_arrays_in);
-        auto [pa_out] = unpack<1>(p_arrays_out);
+        auto [pa_in, pa_noise, pa_mask] = unpack<3>(in);
+        auto [pa_out] = unpack<1>(out);
 
         *pa_out = *pa_in;
 
         hmap::gpu::strata_terrace(*pa_out,
-                                  params.gamma,
-                                  params.seed,
+                                  gamma,
+                                  seed,
                                   pa_mask,
-                                  params.kz,
-                                  params.linear_gamma,
-                                  params.gamma_noise_ratio,
+                                  kz,
+                                  linear_gamma,
+                                  gamma_noise_ratio,
+                                  slope,
+                                  angle,
                                   pa_noise);
       },
       node.cfg().cm_gpu);
 
-  p_out->smooth_overlap_buffers();
+  // --- Post-process
 
-  // post-process
+  p_out->smooth_overlap_buffers();
   post_process_heightmap(node, *p_out, p_in);
 }
 
