435058802216a7cf3a868b642816628a1852dcd0
Otto Link <otto.link.bv@gmail.com>
2026-06-27 23:46:34 +0200

    feat(nodes): add smoothing and standard post-process to DepressionFilling node. Update HighMap

--- Hesiod/src/model/nodes/nodes_function/depression_filling.cpp
+++ Hesiod/src/model/nodes/nodes_function/depression_filling.cpp
@@ -14,51 +14,95 @@ using namespace attr;
 namespace hesiod
 {
 
+// -----------------------------------------------------------------------------
+// Ports & Attributes
+// -----------------------------------------------------------------------------
+
+constexpr const char *P_IN = "input";
+constexpr const char *P_OUT = "output";
+constexpr const char *P_FILL_MAP = "fill map";
+
+constexpr const char *A_REMAP_FILL_MAP = "remap fill map";
+constexpr const char *A_SMOOTHING = "smoothing";
+
+// -----------------------------------------------------------------------------
+// Setup
+// -----------------------------------------------------------------------------
+
 void setup_depression_filling_node(BaseNode &node)
 {
   Logger::log()->trace("setup node {}", node.get_label());
 
-  // port(s)
-  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, "input");
-  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, "output", CONFIG(node));
-  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, "fill map", CONFIG(node));
+  // --- Ports
 
-  // attribute(s)
-  node.add_attr<BoolAttribute>("remap fill map", "remap fill map", true);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::IN, P_IN);
+  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, P_OUT, CONFIG(node));
+  node.add_port<hmap::VirtualArray>(gnode::PortType::OUT, P_FILL_MAP, CONFIG(node));
 
-  // attribute(s) order
-  node.set_attr_ordered_key(
-      {"_GROUPBOX_BEGIN_Main Parameters", "remap fill map", "_GROUPBOX_END_"});
+  // --- Attributes
+
+  // clang-format off
+  node.add_attr<BoolAttribute>(A_SMOOTHING, "Smoothing", false);
+  node.add_attr<BoolAttribute>(A_REMAP_FILL_MAP, "Remap Fill Map", true);
+  // clang-format on
+
+  // --- Attribute(s) order
+
+  node.set_attr_ordered_key({"_GROUPBOX_BEGIN_Main Parameters",
+                             A_SMOOTHING,
+                             A_REMAP_FILL_MAP,
+                             "_GROUPBOX_END_"});
+
+  setup_post_process_heightmap_attributes(node,
+                                          {.add_mix = true, .remap_active_state = false});
 }
+
+// -----------------------------------------------------------------------------
+// Compute
+// -----------------------------------------------------------------------------
 
 void compute_depression_filling_node(BaseNode &node)
 {
   Logger::log()->trace("computing node [{}]/[{}]", node.get_label(), node.get_id());
 
-  hmap::VirtualArray *p_in = node.get_value_ref<hmap::VirtualArray>("input");
+  // --- Inputs / Outputs
 
-  if (p_in)
-  {
-    hmap::VirtualArray *p_out = node.get_value_ref<hmap::VirtualArray>("output");
-    hmap::VirtualArray *p_fill_map = node.get_value_ref<hmap::VirtualArray>("fill map");
+  auto *p_in = node.get_value_ref<hmap::VirtualArray>(P_IN);
+  auto *p_out = node.get_value_ref<hmap::VirtualArray>(P_OUT);
+  auto *p_fill_map = node.get_value_ref<hmap::VirtualArray>(P_FILL_MAP);
 
-    hmap::for_each_tile(
-        {p_out, p_in, p_fill_map},
-        [&node](std::vector<hmap::Array *> p_arrays, const hmap::TileRegion &)
-        {
-          auto [pa_out, pa_in, pa_fill_map] = unpack<3>(p_arrays);
+  if (!p_in || !p_out || !p_fill_map)
+    return;
 
-          *pa_out = *pa_in;
+  // --- Params
 
-          hmap::depression_filling_priority_flood(*pa_out);
+  // clang-format off
+  const auto smoothing      = node.get_attr<BoolAttribute>(A_SMOOTHING);
+  const auto remap_fill_map = node.get_attr<BoolAttribute>(A_REMAP_FILL_MAP);
+  // clang-format on
 
-          *pa_fill_map = *pa_out - *pa_in;
-        },
-        node.cfg().cm_single_array); // forced, not tileable
+  // --- Compute
 
-    if (node.get_attr<BoolAttribute>("remap fill map"))
-      p_fill_map->remap(0.f, 1.f, node.cfg().cm_cpu);
-  }
+  hmap::for_each_tile(
+      {p_out, p_in, p_fill_map},
+      [&](std::vector<hmap::Array *> arrays, const hmap::TileRegion &)
+      {
+        auto [pa_out, pa_in, pa_fill_map] = unpack<3>(arrays);
+
+        *pa_out = *pa_in;
+
+        hmap::depression_filling_priority_flood(*pa_out, smoothing);
+
+        *pa_fill_map = *pa_out - *pa_in;
+      },
+      node.cfg().cm_single_array); // forced, not tileable
+
+  // --- Post-process
+
+  post_process_heightmap(node, *p_out, p_in);
+
+  if (remap_fill_map)
+    p_fill_map->remap(0.f, 1.f, node.cfg().cm_cpu);
 }
 
 } // namespace hesiod
