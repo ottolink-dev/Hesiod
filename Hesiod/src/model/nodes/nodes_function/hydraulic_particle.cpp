b6609fa071c51238bd46b3fc6f1862a3a9452a55
Otto Link <otto.link.bv@gmail.com>
2026-09-14 10:02:21 +0200

    chore: bump HighMap and adjust node default parameters

--- Hesiod/src/model/nodes/nodes_function/hydraulic_particle.cpp
+++ Hesiod/src/model/nodes/nodes_function/hydraulic_particle.cpp
@@ -121,7 +121,7 @@ void setup_hydraulic_particle_node(BaseNode &node)
     node.set_current_category("Simulation");
     add_seed(node, A_SEED, "Seed");
     add_int(node, A_LEVELS, "Levels", 3, 1, 6);
-    add_float(node, A_MIX, "Mix", 0.8f, 0.f, 1.f);
+    add_float(node, A_MIX, "Mix", 0.5f, 0.f, 1.f);
     add_float(node, A_PARTICLE_DENSITY, "Particle Density", 0.5f, 0.f, 4.f);
 
     setup_common_particle_attributes(node);
