819455a09dd47943779381820256e2f073590833
Otto Link <otto.link.bv@gmail.com>
2026-09-21 20:44:38 +0200

    Update Gamma Exponent max value in GammaCorrectionLocal node

--- Hesiod/src/model/nodes/nodes_function/gamma_correction_local.cpp
+++ Hesiod/src/model/nodes/nodes_function/gamma_correction_local.cpp
@@ -42,7 +42,7 @@ void setup_gamma_correction_local_node(BaseNode &node)
   // --- Attributes
 
   add_float(node, A_RADIUS, "Radius", 0.05f, 0.01f, 0.2f);
-  add_float(node, A_GAMMA, "Gamma Exponent", 2.f, 0.01f, 10.f);
+  add_float(node, A_GAMMA, "Gamma Exponent", 2.f, 0.01f, 4.f);
   add_float(node, A_K, "Smoothing", 0.1f, 0.f, 0.5f);
 
   // --- Attribute(s) order
